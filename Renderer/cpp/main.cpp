2f97f57d86587aa681528e929a845f1ad5627748
Erick Dias Piva <erickdpiva@gmail.com>
2023-02-18 22:13:01 -0300

    just conrtruct the app

--- Renderer/cpp/main.cpp
+++ Renderer/cpp/main.cpp
@@ -1,34 +1,16 @@
-#include <stdint.h>
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
+#include <app/first_app.hpp>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    glfwInit();
+    rot::FirstApp app{};
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *window =
-        glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-
-    auto test = matrix * vec;
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    try {
+        app.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+        return 1;
     }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
     return 0;
 }
