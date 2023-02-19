9d9dbdfefa764551431171c347abbd67567eeda7
Erick Dias Piva <erickdpiva@gmail.com>
2023-02-18 22:16:53 -0300

    removing compile_commands from versioning

--- Renderer/cpp/app/first_app.cpp
+++ Renderer/cpp/app/first_app.cpp
@@ -1,10 +1,13 @@
 #include "first_app.hpp"
 #include "GLFW/glfw3.h"
+#include "profiler/profiler.hpp"
 #include "window/window.hpp"
 
 namespace rot {
 void FirstApp::run() {
+    PROF_SCOPED(PROF_LVL_ALL, __func__);
     while (!rotWindow.shouldClose()) {
+        PROF_SCOPED(PROF_LVL_ALL, "Frame");
         glfwPollEvents();
     }
 }
--- Renderer/cpp/premake5.lua
+++ Renderer/cpp/premake5.lua
@@ -6,19 +6,19 @@ configurations({ "Debug", "Release" })
 project("RotRenderer")
 kind("ConsoleApp")
 language("C++")
-cppdialect("C++17")
+cppdialect("C++20")
 targetdir("bin/%{cfg.buildvfg}")
 
 system("Windows")
 architecture("x86_64")
 
-files({ "main.cpp", "window/*", "app/*" })
-includedirs({ "include", "C:/VulkanSDK/1.3.239.0/Include", "./" })
+files({ "main.cpp", "profiler/*", "window/*", "app/*" })
+includedirs({ "include", "C:/VulkanSDK/1.3.239.0/Include", "./", "C:/llvm-prof/llvm-project/llvm/include" })
 libdirs({ "C:/VulkanSDK/1.3.239.0/Lib", "vendor/glfw-3.3.8/lib-vc2022" })
 links({ "vulkan-1", "glfw3" })
 
 filter("configurations:Debug")
-defines({ "DEBUG" })
+defines({ "DEBUG", "GENERIC_PROFILER" })
 symbols("On")
 
 filter("configurations:Release")
