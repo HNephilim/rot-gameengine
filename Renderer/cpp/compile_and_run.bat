a0583ee8bc23e4c903a164c227d6f5c1925545ed
Erick Dias Piva <erickdpiva@gmail.com>
2023-02-18 22:14:33 -0300

    made sure the project is updated before compiling

--- Renderer/cpp/compile_and_run.bat
+++ Renderer/cpp/compile_and_run.bat
@@ -1,2 +1,5 @@
+premake5 vs2022
+premake5 export-compile-commands
+move .\compile_commands\debug.json .\compile_commands.json
 msbuild /p:Configuration=Debug /p:Plataform=x64 
 .\bin\RotRenderer.exe
