edf2be16c73865b4e06e439562c4f7c01df62a2d
Erick Dias Piva <erickdpiva@gmail.com>
2023-02-18 22:13:25 -0300

    adding root to include directory. Adding app

--- Renderer/cpp/premake5.lua
+++ Renderer/cpp/premake5.lua
@@ -12,8 +12,8 @@ targetdir("bin/%{cfg.buildvfg}")
 system("Windows")
 architecture("x86_64")
 
-files({ "main.cpp", "window/*" })
-includedirs({ "include", "C:/VulkanSDK/1.3.239.0/Include" })
+files({ "main.cpp", "window/*", "app/*" })
+includedirs({ "include", "C:/VulkanSDK/1.3.239.0/Include", "./" })
 libdirs({ "C:/VulkanSDK/1.3.239.0/Lib", "vendor/glfw-3.3.8/lib-vc2022" })
 links({ "vulkan-1", "glfw3" })
 
