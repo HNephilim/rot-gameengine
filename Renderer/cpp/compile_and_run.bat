84fc88c096a74368d38dbe9b84c2623e599860d0
Erick Dias Piva <erickdpiva@gmail.com>
2023-02-23 12:21:28 -0300

    organazing git

--- Renderer/cpp/compile_and_run.bat
+++
@@ -1,5 +0,0 @@
-premake5 vs2022
-premake5 export-compile-commands
-move .\compile_commands\debug.json .\compile_commands.json
-msbuild /p:Configuration=Debug /p:Plataform=x64 
-.\bin\RotRenderer.exe
